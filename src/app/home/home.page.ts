55c841afedc122f71a57e18a24b9a14e58e67b88
Álvaro <agomez@tribalyte.com>
2020-01-24 14:45:28 +0100

    New data source for share example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { Observable, Subject, of, } from 'rxjs';
-import { share, shareReplay, merge } from 'rxjs/operators'
+import { share, shareReplay, merge, flatMap, map } from 'rxjs/operators'
+
+interface IResponse {
+    data: string;
+    status: string;
+}
 
 @Component({
     selector: 'app-home',
@@ -9,7 +15,9 @@ import { share, shareReplay, merge } from 'rxjs/operators'
 })
 export class HomePage {
 
-    private readonly URL = "https://www.google.es/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png";
+    private readonly STATUS_URL = "http://5.56.60.217:3001/profile-image/0x41E4e56603bF37a03Bb9DDD88A64b3068052b82a/status";
+    private readonly IMAGE_URL = "http://5.56.60.217:3001/profile-image/0x41E4e56603bF37a03Bb9DDD88A64b3068052b82a/";
+    private readonly STATUS_OK = "OK";
 
     public observable: Observable<string>;
     public textBox: string;
@@ -17,20 +25,20 @@ export class HomePage {
 
     private subject = new Subject<string>()
 
-    ionViewWillEnter() {
-        this.observable = of<string>(this.URL)
+    constructor(private http: HttpClient){
+        this.observable = this.http.get(this.STATUS_URL)
         .pipe(
+            map((response: IResponse)=> {
+                let imageUrl = (response && response.status === this.STATUS_OK) ? this.IMAGE_URL : "";
+                return imageUrl;
+            }),
             merge(this.subject),
-            shareReplay()
+            shareReplay(1)
             );
     }
 
     public update() {
         this.subject.next(this.textBox);
-    }
-
-    public do() {
-        //this.observable.subscribe(value => console.log(value));
     }
 
 }
