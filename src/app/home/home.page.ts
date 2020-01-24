2050a5d1a6be65a8e52c4eeda49efdb2d59e1e19
Álvaro <agomez@tribalyte.com>
2020-01-24 14:49:15 +0100

    Students version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,26 +19,13 @@ export class HomePage {
     private readonly IMAGE_URL = "http://5.56.60.217:3001/profile-image/0x41E4e56603bF37a03Bb9DDD88A64b3068052b82a/";
     private readonly STATUS_OK = "OK";
 
-    public observable: Observable<string>;
     public textBox: string;
     public tabId = 0;
 
-    private subject = new Subject<string>()
-
     constructor(private http: HttpClient){
-        this.observable = this.http.get(this.STATUS_URL)
-        .pipe(
-            map((response: IResponse)=> {
-                let imageUrl = (response && response.status === this.STATUS_OK) ? this.IMAGE_URL : "";
-                return imageUrl;
-            }),
-            merge(this.subject),
-            shareReplay(1)
-            );
     }
 
     public update() {
-        this.subject.next(this.textBox);
     }
 
 }
